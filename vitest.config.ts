c439b6fe780ab13d64766895ce67058ee6a645b5
Ross Gardler <ross@gardler.org>
2025-12-25 00:40:58 -0800

    Resolve vitest config conflict: accept upstream

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['tests/**/*.{test,e2e}.ts'],
-    exclude: ['**/node_modules/**', '**/dist/**'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/worktree_*/**'],
   },
 });
